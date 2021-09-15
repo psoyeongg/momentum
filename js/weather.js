f5dd6598fe63967c1db399c8fc435bf4ce7f1baa
sypark96 <sypark96@nkia.co.kr>
2021-09-15 16:56:10 +0900

    날씨 추가

---
+++ js/weather.js
@@ -0,0 +1,21 @@
+const API_KEY = "6239d2409dee0a44224b2f9e46cce1a4";
+
+function onGeoOk(position) {
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const weather = document.querySelector("#weather span:first-child");
+      const city = document.querySelector("#weather span:last-child");
+      city.innerText = data.name;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
+}
+
+function onGeoError() {
+  alert("널 찾을 수 없어. 날씨도 찾을 수 없어");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
