bd970b56fc13a0b578a831013fbf46f2c10bd0e7
sypark96 <sypark96@nkia.co.kr>
2021-09-15 17:11:47 +0900

    스타일 적용

--- js/background.js
+++ js/background.js
@@ -3,6 +3,7 @@ const images = ["0.jpg", "1.jpg", "2.jpg"];
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
 const bgImage = document.createElement("img");
+bgImage.id = "bgImage";
 
 bgImage.src = `img/${chosenImage}`;
 
